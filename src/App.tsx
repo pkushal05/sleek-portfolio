afe3648ee82570fc0c65be696db07ee3c8d8d82e
pkushal05 <pkushal1052@gmail.com>
2026-01-02 18:25:18 -0500

    Started little bit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { Tooltip, TooltipPopup, TooltipTrigger, TooltipProvider } from "@/components/ui/tooltip";
+import { useState, useEffect } from "react";
+
 const App = () => {
+
+  const [isDark, setIsDark] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDark]);
+
   return (
     <div className="h-screen w-screen flex items-center justify-center flex-col gap-10">
-      <h1 className="text-9xl">Kushal Patel</h1>
+      <h1 className="text-9xl font-bold">Satoshi</h1>
       <div className="h-3 w-3 bg-green-400 rounded-full relative">
         <span className="h-3 w-3 bg-green-400 rounded-full absolute left-0 top-0 animate-ping"></span>
       </div>
       <div className="container bg-amber-400 h-10 w-10 rounded-full"></div>
+      <TooltipProvider delay={0}>
+        <Tooltip>
+          <TooltipTrigger
+            render={
+              <Button
+                className=""
+                size="xl"
+                variant={"default"}
+                onClick={() => setIsDark(!isDark)}
+              >
+                {isDark ? "Light" : "Dark"}
+              </Button>
+            }
+          ></TooltipTrigger>
+          <TooltipPopup>Click to Toogle Theme</TooltipPopup>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "selector",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "system-ui", "sans-serif"],
+        grotesk: ["Space Grotesk", "system-ui", "sans-serif"],
+      },
+      borderRadius: {
+        sm: "calc(var(--radius) - 4px)",
+        md: "calc(var(--radius) - 2px)",
+        lg: "var(--radius)",
+        xl: "calc(var(--radius) + 4px)",
+        "2xl": "calc(var(--radius) + 8px)",
+        "3xl": "calc(var(--radius) + 12px)",
+        "4xl": "calc(var(--radius) + 16px)",
+      },
+    },
+  },
+  plugins: [],
+};
